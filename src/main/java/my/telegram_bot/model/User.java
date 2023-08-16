f5e9aee39bdd05328002dae22f1b687e209f5add
Yaroslav Agapov <euraks@yandex.ru>
2023-08-16 09:29:23 +0500

    feat: Add User Time Commands

--- src/main/java/my/telegram_bot/model/User.java
+++ src/main/java/my/telegram_bot/model/User.java
@@ -1,4 +1,37 @@
 package my.telegram_bot.model;
 
+import lombok.Data;
+import my.telegram_bot.service.enums.ServiceCommands;
+
+import java.time.LocalTime;
+
+@Data
 public class User {
+
+    private Long id;
+
+    private String firstName;
+
+    private Boolean isBot;
+
+    private String lastName;
+
+    private String userName;
+
+    private String languageCode;
+
+    private Boolean canJoinGroups;
+
+    private Boolean canReadAllGroupMessages;
+
+    private Boolean supportInlineQueries;
+
+    private ServiceCommands commands = ServiceCommands.START;
+
+    private LocalTime timeLastCommands ;
+
+    public void setCommands(ServiceCommands commands) {
+        timeLastCommands = LocalTime.now();
+        this.commands = commands;
+    }
 }
--- src/main/java/my/telegram_bot/service/UserService.java
+++ src/main/java/my/telegram_bot/service/UserService.java
@@ -2,9 +2,11 @@ package my.telegram_bot.service;
 
 import lombok.extern.log4j.Log4j;
 import my.telegram_bot.model.User;
+import my.telegram_bot.service.enums.ServiceCommands;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+import java.time.LocalTime;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,9 +18,15 @@ public class UserService {
     public User get(Update update) {
         Long userId = getId( update );
         if (userMap.containsKey( userId )) {
+            User user = userMap.get( userId );
+            if (user.getTimeLastCommands().isAfter( LocalTime.now().plusMinutes( 10 ) )){
+                user.setCommands( ServiceCommands.START );
+                userMap.put( userId,user );
+            }
             return userMap.get( userId );
         } else {
             User newUser = new User();
+            newUser.setCommands( ServiceCommands.START );
             userMap.put( userId,newUser );
             return newUser;
         }
